e4caa0aa57296324f50b67362d023501b9787f33
docterlei <1101668549@qq.com>
2024-04-10 10:59:31 +0800

    j

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -13,18 +13,11 @@ export default defineUserConfig<DefaultThemeOptions>({
     // logo: 'https://vuejs.org/images/logo.png',
     navbar: [
       { text: '前端', link: '/frontend/js/basics/context' },
+      { text: '后端', link: '/backend/' },
       { text: '网络', link: '/netWork/http/cache' },
-      // { text: '后端', link: '/backend/' },
+      { text: '其它', link: '/other/command' },
       // { text: '算法', link: '/algorithm/' },
       // { text: '设计模式', link: '/designPattern/' },
-      { text: '其它', link: '/other/command' },
-      { 
-          text: 'lgh博客', 
-          children: [
-              // { text: 'Github', link: 'https://github.com/mqyqingfeng' },
-              // { text: '掘金', link: 'https://juejin.cn/user/712139234359182/posts' }
-          ]
-      },
     ],
     sidebar: {
       '/frontend/':[
@@ -51,14 +44,7 @@ export default defineUserConfig<DefaultThemeOptions>({
             '/frontend/js/call-apply-bind'
           ]
         },
-        {
-          text: 'TS',
-          collapsible: true, 
-          children: [
-            '/frontend/ts/info',
-            '/frontend/ts/typeManipulation/keyof',
-          ]
-        }],
+       ],
 
       '/netWork/':[
         {
@@ -86,24 +72,10 @@ export default defineUserConfig<DefaultThemeOptions>({
         }],
 
       '/backend/':[{
-          text: '前端',
+          text: '后端',
           collapsible: true, 
           children: [
-            '/backend/closure'
-          ]
-        }],
-      '/algorithm/':[{
-          text: '前端',
-          collapsible: true,
-          children: [
-            '/algorithm/closure'
-          ]
-        }],
-      '/designPattern/':[{
-          text: '前端',
-          collapsible: true, 
-          children: [
-            '/designPattern/closure'
+            '/mysql/closure'
           ]
         }],
       '/other/':[{
--- docs/other/regexp.md
+++ docs/other/regexp.md
@@ -2,5 +2,3 @@
 
 [regulex](https://jex.im/regulex/#!flags=&re=%5E(a%7Cb)*%3F%24)
 
-### ss
-<iframe id="iframe" height=500 width=80% frameborder=0 allowfullcreen="true" src="/english/index.html"></iframe>
